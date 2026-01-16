a2211eb5674df9977ffee0484c7c1b2a4f17485e
mark pancake <mark@sky-cloud.net>
2026-01-16 20:02:04 +0800

    Initial commit for SEO refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://opensecflow.com',
+  site: 'https://opensecflow.io',
   integrations: [
     tailwind(),
     mdx(),
